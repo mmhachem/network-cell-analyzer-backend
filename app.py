3700d1379780c56636742aaec1afd90bd20a7e1a
mmhachem <mmh156@mail.aub.edu>
2025-03-22 21:30:06 +0200

    Add Procfile and confirm requirements

--- app.py
+++ app.py
@@ -111,5 +111,4 @@ def home():
 
 # Run app
 if __name__ == '__main__':
-    from os import environ
-    app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000)
